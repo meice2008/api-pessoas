08fcb52aaa09be96f1756526ab7fefc7e43a2e63
Meice Silva <meice2008@gmail.com>
2026-05-29 19:01:56 -0300

    adicionando repositorio pessoa e sua interface

---
+++ WebApplication1/Interfaces/IPessoaRepository.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication1.Entities;
+
+namespace WebApplication1.Interfaces
+{
+    public interface IPessoaRepository
+    {
+        Task<List<Pessoa>> ObterTodosAsync();
+
+        Task<Pessoa?> ObterPorIdAsync(int id);
+
+        Task AdicionarAsync(Pessoa pessoa);
+
+        Task AtualizarAsync(Pessoa pessoa);
+
+        Task RemoverAsync(Pessoa pessoa);
+    }
+}
---
+++ WebApplication1/Repositories/PessoaRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Entities;
+using WebApplication1.Interfaces;
+
+namespace WebApplication1.Repositories
+{
+    public class PessoaRepository : IPessoaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PessoaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Pessoa>> ObterTodosAsync()
+        {
+            return await _context.Pessoas
+                .Include(p => p.Telefones)
+                .ToListAsync();
+        }
+
+        public async Task<Pessoa?> ObterPorIdAsync(int id)
+        {
+            return await _context.Pessoas
+                .Include(p => p.Telefones)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task AdicionarAsync(Pessoa pessoa)
+        {
+            await _context.Pessoas.AddAsync(pessoa);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AtualizarAsync(Pessoa pessoa)
+        {
+            _context.Pessoas.Update(pessoa);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoverAsync(Pessoa pessoa)
+        {
+            _context.Pessoas.Remove(pessoa);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
